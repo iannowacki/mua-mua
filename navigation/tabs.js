3e2fb4a487957c885dfc2380224ffa5fcbfc708e
iannowacki <iannowacki@hotmail.com>
2021-08-07 00:39:22 +0100

    changes

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -11,10 +11,8 @@ const Tab = createMaterialBottomTabNavigator();
 
 const Tabs = () => {
     return(
-        <Tab.Navigator inactiveColor='#FFFFFF'>
-            <Tab.Screen name="Calendar" component={CalendarViewScreenNavigator} options={{headerShown: false, tabBarLabel: 'Calendar', tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="calendar-month-outline" color={color} size={26} />
-          ),}}  />
+        <Tab.Navigator inactiveColor='#FFFFFF' backBehaviour = "none" >
+            <Tab.Screen name="Calendar" component={CalendarViewScreenNavigator} options={{headerShown: false, tabBarLabel: 'Calendar', tabBarIcon: ({ color }) => (<MaterialCommunityIcons name="calendar-month-outline" color={color} size={26} />),}}  />
             <Tab.Screen name="Messages" component={MessengerScreenNavigator}  options={{headerShown: false, tabBarLabel: 'Messages',
           tabBarIcon: ({ color }) => (
             <MaterialCommunityIcons name="email" color={color} size={26} />
@@ -23,7 +21,7 @@ const Tabs = () => {
           tabBarIcon: ({ color }) => (
             <MaterialCommunityIcons name="gamepad-round" color={color} size={26} />
           ),}} />
-            <Tab.Screen name="View Booking" component={SearchScreenNavigator} options={{headerShown: false, tabBarLabel: 'View Bookings',
+            <Tab.Screen name="View Bookings" component={SearchScreenNavigator} options={{headerShown: false, tabBarLabel: 'View Bookings',
           tabBarIcon: ({ color }) => (
             <MaterialCommunityIcons name="magnify" color={color} size={26} />
           ),}} />
